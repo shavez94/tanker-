1cfe6778baa7822d9aa3050ababd472f121e4d0f
shavez94 <58049360+shavez94@users.noreply.github.com>
2021-09-28 01:54:51 +0530

    2

--- Cannon/Assets/player.cs
+++ Cannon/Assets/player.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class player : MonoBehaviour
+public class player : MonoBehaviour,IDragHandler,IPointerUpHandler,IPointerDownHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public RectTransform pad;
+
+    public Transform target;
+    Vector3 move;
+    public float movespeed;
+
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        transform.position = eventData.position;
+        transform.localPosition = Vector2.ClampMagnitude(eventData.position - (Vector2)pad.position, pad.rect.width * 0.5f);
+        move = new Vector3(transform.localPosition.x, 0, transform.localPosition.y).normalized;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        transform.localPosition = Vector3.zero;
+        move = Vector3.zero;
+        StartCoroutine("PlayerMove");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerDown(PointerEventData eventData)
     {
-        
+        StartCoroutine("PlayerMove");
     }
+
+    IEnumerator PlayerMove()
+    {
+        while(true)
+        {
+            target.Translate(move * movespeed * Time.deltaTime, Space.World);
+
+            if (move != Vector3.zero)
+                target.rotation = Quaternion.Slerp(target.rotation, Quaternion.LookRotation(move), 5 * Time.deltaTime);
+
+            yield return null;
+        }
+    }
+
+
+
+
 }
